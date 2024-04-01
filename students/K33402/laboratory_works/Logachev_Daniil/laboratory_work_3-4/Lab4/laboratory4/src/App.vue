8ce61f9adb072bd940ebecc2339be1980c0bb023
DaniilLog <logachev.daniil.K3140@gmail.com>
2024-04-01 19:59:50 +0300

    К33402 Логачев Даниил ЛР3
    Lab3-4 done!

---
+++ students/K33402/laboratory_works/Logachev_Daniil/laboratory_work_3-4/Lab4/laboratory4/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {RouterView} from 'vue-router'
+import NavBar from '@/components/NavBar.vue';
+</script>
+
+<template>
+  <NavBar />
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+
+<style scoped>
+  main {
+    margin-left: 375px;
+    width: calc(100vw - 375px);
+  }
+</style>
